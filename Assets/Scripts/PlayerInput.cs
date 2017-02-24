a6ea4372495d6327d198a534d4f31665fa8fb9f2
Oto Luksa <oto.luksa@gmail.com>
2017-02-24 15:26:45 +0100

    Added player character and animation

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour {
+
+	public static Vector2 GetMovementInput ()
+	{
+	    Vector2 movementInput;
+
+        #if UNITY_EDITOR || UNITY_STANDALONE_WIN || UNITY_WEBGL
+
+            movementInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        #endif
+
+        return movementInput;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    private Rigidbody2D _rigidBody;
+    private Animator _animator;
+
+    // Use this for initialization
+    void Start ()
+    {
+        _rigidBody = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    Vector2 inputVector = PlayerInput.GetMovementInput();
+        if (inputVector != Vector2.zero)
+        {
+            _animator.SetFloat("inputX", inputVector.x);
+            _animator.SetFloat("inputY", inputVector.y);
+            _animator.SetBool("isWalking", true);
+        }
+        else
+        {
+            _animator.SetBool("isWalking", false);
+        }
+
+        _rigidBody.MovePosition(_rigidBody.position + inputVector.normalized * Time.deltaTime);
+    }
+}
